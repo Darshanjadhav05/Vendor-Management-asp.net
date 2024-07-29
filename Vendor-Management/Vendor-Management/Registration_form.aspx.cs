5a54e6d7b84231b8e7e21ebb44a6acc14642584e
Darshanjadhav05 <darshanmjadhav5@gmail.com>
2024-07-29 21:21:34 +0530

    add missing fields and validations

--- Vendor-Management/Vendor-Management/Registration_form.aspx.cs
+++ Vendor-Management/Vendor-Management/Registration_form.aspx.cs
@@ -12,7 +12,7 @@ namespace Vendor_Management
         protected void Page_Load(object sender, EventArgs e)
         {
         }
-
+      
         protected void NextButton1_Click(object sender, EventArgs e)
         {
         }
